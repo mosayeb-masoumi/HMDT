38b468f485b84fdca7a05413d95695fba9f49053
mosayeb_masoumi <mosayeb.masoumi.co@gmail.com>
2020-01-05 16:50:54 +0330

    first commit

--- app/src/main/java/com/rahbarbazaar/checkpanel/utilities/App.java
+++ app/src/main/java/com/rahbarbazaar/checkpanel/utilities/App.java
@@ -11,7 +11,7 @@ import io.fabric.sdk.android.Fabric;
 public class App extends Application {
 
 
-    private RefWatcher refWatcher;
+//    private RefWatcher refWatcher;
 
 
     @Override
@@ -22,16 +22,16 @@ public class App extends Application {
 
 
 
-      //todo delete leakCanary
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return;
-        }
-        refWatcher = LeakCanary.install(this);
+//      //todo delete leakCanary
+//        if (LeakCanary.isInAnalyzerProcess(this)) {
+//            return;
+//        }
+//        refWatcher = LeakCanary.install(this);
     }
 
 
-    public static RefWatcher getRefWatcher(Context context) {
-        App application = (App) context.getApplicationContext();
-        return application.refWatcher;
-    }
+//    public static RefWatcher getRefWatcher(Context context) {
+//        App application = (App) context.getApplicationContext();
+//        return application.refWatcher;
+//    }
 }
