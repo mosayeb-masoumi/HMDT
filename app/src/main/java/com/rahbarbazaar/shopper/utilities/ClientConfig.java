3140922bca601b6d46ad338722997fe50b9ad312
mosayeb_masoumi <mosayeb.masoumi.co@gmail.com>
2020-03-04 12:00:58 +0330

    test git

--- app/src/main/java/com/rahbarbazaar/shopper/utilities/ClientConfig.java
+++ app/src/main/java/com/rahbarbazaar/shopper/utilities/ClientConfig.java
@@ -5,4 +5,8 @@ public class ClientConfig {
     public static String ServerURL = "https://test.rahbarbazar.com/cp/api/v1/";
 //        public static String ServerURL = "https://cp.rahbarbazar.com/api/v1/";
     public static String Html_URL = "https://test.rahbarbazar.com/cp/";
+
+    //test
+
+
 }
